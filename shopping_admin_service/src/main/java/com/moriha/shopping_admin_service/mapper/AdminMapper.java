1c714ac632c6d53d954ba82aa6ca4132c7079e7f
vHelios3241 <3695748486@qq.com>
2026-08-02 22:23:40 +0800

    管理员模块：给管理员添加角色

--- shopping_admin_service/src/main/java/com/moriha/shopping_admin_service/mapper/AdminMapper.java
+++ shopping_admin_service/src/main/java/com/moriha/shopping_admin_service/mapper/AdminMapper.java
@@ -4,14 +4,17 @@ package com.moriha.shopping_admin_service.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.moriha.common.pojo.Admin;
 import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Param;
 
 public interface AdminMapper extends BaseMapper<Admin> {
 
-    // 删除管理员角色
+    // 删除管理员所有角色
     @Delete("delete from bz_admin_role where aid=#{aid}")
-    void deleteAdminRole(Long aid);
+    void deleteAdminAllRole(Long aid);
 
-    // 根据id查询管理员
+    // 根据id查询管理员 （角色+权限）
     Admin findById(Long id);
+
+    //给管理员添加角色
+    void addRoleToAdmin(@Param("aid") Long aid, @Param("rid") Long rid);
 }
