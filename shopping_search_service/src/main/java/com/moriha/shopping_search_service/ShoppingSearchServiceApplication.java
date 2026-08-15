d9f5400b4c8373472dc05e66ea498fa2b5efad11
vHelios3241 <3695748486@qq.com>
2026-08-15 14:58:16 +0800

    商品搜索：搜索服务接口

---
+++ shopping_search_service/src/main/java/com/moriha/shopping_search_service/ShoppingSearchServiceApplication.java
@@ -0,0 +1,31 @@
+package com.moriha.shopping_search_service;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@EnableDubbo
+@RefreshScope
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class ShoppingSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingSearchServiceApplication.class, args);
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
+
