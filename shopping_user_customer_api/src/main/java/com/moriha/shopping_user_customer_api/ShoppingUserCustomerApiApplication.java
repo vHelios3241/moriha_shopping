095993c945cd2f6a5271790c962bb2b54784ab4c
vHelios3241 <3695748486@qq.com>
2026-08-24 11:54:37 +0800

    商城用户:创建网站用户模块

---
+++ shopping_user_customer_api/src/main/java/com/moriha/shopping_user_customer_api/ShoppingUserCustomerApiApplication.java
@@ -0,0 +1,18 @@
+package com.moriha.shopping_user_customer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient //向注册中心注册该服务
+@RefreshScope // 配置动态刷新
+public class ShoppingUserCustomerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingUserCustomerApiApplication.class, args);
+    }
+
+}
