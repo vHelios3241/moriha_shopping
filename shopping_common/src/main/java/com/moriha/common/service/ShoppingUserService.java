6acace7549f316dc399fba2f0eb6b9e37978e806
vHelios3241 <3695748486@qq.com>
2026-08-25 14:15:29 +0800

    商城用户:用户服务接口

---
+++ shopping_common/src/main/java/com/moriha/common/service/ShoppingUserService.java
@@ -0,0 +1,26 @@
+package com.moriha.common.service;
+
+import com.moriha.common.pojo.ShoppingUser;
+
+/*
+ * 商城用户服务
+ */
+public interface ShoppingUserService {
+    // 注册时向redis保存手机号+验证码
+    void saveRegisterCheckCode(String phone, String code);
+    // 注册时验证手机号+验证码
+    void registerCheckCode(String phone, String code);
+    // 用户注册
+    void register(ShoppingUser user);
+
+    // 用户名+密码 登录
+    String loginPassword(String username, String password);
+    // 手机号+验证码 登录
+    String loginCheckCode(String phone, String checkCode);    // 登录时向redis保存手机号+验证码
+    void saveLoginCheckCode(String phone,String checkCode);
+
+    // 获取登录用户名
+    String getName(String token);
+    // 根据id获取用户
+    ShoppingUser getLoginUser(Long id);
+}
