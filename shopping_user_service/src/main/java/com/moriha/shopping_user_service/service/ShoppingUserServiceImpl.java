de56c9a0d1c9ee335fd3b394143e4f7bb7a37182
vHelios3241 <3695748486@qq.com>
2026-08-26 11:28:14 +0800

    商城用户:发送注册验证码功能

--- shopping_user_service/src/main/java/com/moriha/shopping_user_service/service/ShoppingUserServiceImpl.java
+++ shopping_user_service/src/main/java/com/moriha/shopping_user_service/service/ShoppingUserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.moriha.shopping_user_service.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.moriha.common.pojo.ShoppingUser;
 import com.moriha.common.result.BusException;
 import com.moriha.common.result.CodeEnum;
 import com.moriha.common.service.ShoppingUserService;
+import com.moriha.common.util.Md5Util;
+import com.moriha.shopping_user_service.mapper.ShoppingUserMapper;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -17,7 +20,8 @@ public class ShoppingUserServiceImpl implements ShoppingUserService {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    /**
+
+    /*
      * 注册时 向redis保存手机号+验证码
      * @param phone
      * @param code
@@ -29,21 +33,32 @@ public class ShoppingUserServiceImpl implements ShoppingUserService {
         valueOperations.set("registerCode:" + phone, code, 300, TimeUnit.SECONDS);
     }
 
-    /**
+    /*
      * 注册时 验证手机号+验证码
      * @param phone
      * @param code
      */
     @Override
     public void registerCheckCode(String phone, String code) {
-
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String redisCode = (String) valueOperations.get("registerCode:" + phone);
+        if(!code.equals(redisCode)){
+            throw new BusException(CodeEnum.REGISTER_CODE_ERROR);
+        }
     }
 
+    /*
+     * 注册
+     * @param shoppingUser
+     */
     @Override
-    public void register(ShoppingUser user) {
+    public void register(ShoppingUser shoppingUser) {
 
     }
 
+    /*
+     * 用户名+密码 登录
+     */
     @Override
     public String loginPassword(String username, String password) {
         return "";
