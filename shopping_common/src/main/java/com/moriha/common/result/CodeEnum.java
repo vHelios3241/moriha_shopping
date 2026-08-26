52c702c534e02c337c38aadfb0bf3e3b5c1b3818
vHelios3241 <3695748486@qq.com>
2026-08-26 11:33:42 +0800

    商城用户:用户注册

--- shopping_common/src/main/java/com/moriha/common/result/CodeEnum.java
+++ shopping_common/src/main/java/com/moriha/common/result/CodeEnum.java
@@ -12,7 +12,10 @@ public enum CodeEnum {
     PARAMETER_ERROR(601, "参数异常"),
     INSERT_PRODUCT_TYPE_ERROR(602, "3级商品不能添加子级别"),
     DELETE_PRODUCT_TYPE_ERROR(603,"该类型有子类型，禁止删除"),
-    UPLOAD_FILE_ERROR(604, "上传文件异常")
+    UPLOAD_FILE_ERROR(604, "上传文件异常"),
+    REGISTER_CODE_ERROR(605, "验证码错误"),
+    REGISTER_REPEAT_PHONE_ERROR(606, "手机号已存在"),
+    REGISTER_REPEAT_NAME_ERROR(607, "用户名已存在"),
     ;
 
     private final Integer code;
