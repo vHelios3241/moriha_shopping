388ea29427998a578b15cd453920a4808ba40130
vHelios3241 <3695748486@qq.com>
2026-09-02 17:25:24 +0800

    购买商品:监听消息的配置

--- shopping_cart_service/src/main/java/com/moriha/shopping_cart_service/service/CartServiceImpl.java
+++ shopping_cart_service/src/main/java/com/moriha/shopping_cart_service/service/CartServiceImpl.java
@@ -6,6 +6,7 @@ import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @DubboService
+@Service
 public class CartServiceImpl implements CartService {
 
     @Autowired
