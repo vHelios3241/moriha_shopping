1c40b9781754e50eaa85e3026048e1aba6a09fb0
vHelios3241 <3695748486@qq.com>
2026-09-03 20:47:26 +0800

    购买商品：地址服务接口

---
+++ shopping_common/src/main/java/com/moriha/common/service/AddressService.java
@@ -0,0 +1,36 @@
+package com.moriha.common.service;
+
+import com.moriha.common.pojo.Address;
+import com.moriha.common.pojo.Area;
+import com.moriha.common.pojo.City;
+import com.moriha.common.pojo.Province;
+
+import java.util.List;
+
+public interface AddressService {
+
+    // 查询所有省份
+    List<Province> findAllProvince();
+
+    // 查询省份下的城市
+    List<City> findCityByProvince(Long provinceId);
+
+    // 查询城市下的区
+    List<Area> findAreaByCity(Long cityId);
+
+    // 增加地址
+    void add(Address address);
+
+    // 修改地址
+    void update(Address address);
+
+    // 根据id获取地址
+    Address findById(Long id);
+
+    // 删除地址
+    void delete(Long id);
+
+    // 查询登录用户的所有地址
+    List<Address> findByUser(Long userId);
+
+}
