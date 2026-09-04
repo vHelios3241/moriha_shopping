f9f625fcde0fabf5b075f08d9253e676c62fccee
vHelios3241 <3695748486@qq.com>
2026-09-04 19:25:43 +0800

    购买商品：创建订单相关模块

---
+++ shopping_order_customer_api/src/main/java/com/moriha/shopping_order_customer_api/ShoppingOrderCustomerApiApplication.java
@@ -0,0 +1,17 @@
+package com.moriha.shopping_order_customer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@EnableDiscoveryClient
+@RefreshScope
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ShoppingOrderCustomerApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingOrderCustomerApiApplication.class, args);
+    }
+
+}
